99680643f3821aed5eb9e8acc226ab509fc3830d
Ashish Tripathi <ashishtripathipara@gmail.com>
2025-08-20 23:54:28 +0530

    toggle_on_mobile_fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,62 @@
 import React, { useState } from "react";
+import ThemeToggle from "./ThemeToggle";
 import "./Navbar.css";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <h1>Ashish Tripathi</h1>
 
-      {/* Hamburger icon */}
-      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? "✖" : "☰"} {/* Switch between ☰ and ✖ */}
+      {/* Right side: ThemeToggle + Hamburger */}
+      <div className="right-icons">
+        <ThemeToggle />
+        <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? "✖" : "☰"}
+        </div>
       </div>
 
       {/* Links */}
       <ul className={isOpen ? "active" : ""}>
         <li>
-          <a href="#about">About</a>
+          <a href="#about" onClick={handleLinkClick}>
+            About
+          </a>
         </li>
         <li>
-          <a href="#skills">Skills</a>
+          <a href="#skills" onClick={handleLinkClick}>
+            Skills
+          </a>
         </li>
         <li>
-          <a href="#experience">Experience</a>
+          <a href="#experience" onClick={handleLinkClick}>
+            Experience
+          </a>
         </li>
         <li>
-          <a href="#projects">Projects</a>
+          <a href="#projects" onClick={handleLinkClick}>
+            Projects
+          </a>
         </li>
         <li>
-          <a href="#education">Education</a>
+          <a href="#education" onClick={handleLinkClick}>
+            Education
+          </a>
         </li>
         <li>
-          <a href="#interests">Interest</a>
+          <a href="#interests" onClick={handleLinkClick}>
+            Interest
+          </a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a href="#contact" onClick={handleLinkClick}>
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
